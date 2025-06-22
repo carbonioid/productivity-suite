96aa14b77e1c3cfb0b8e360c54452cfb6dcf20e2
BagelDogM <bageldogm@gmail.com>
2025-06-22 17:50:22 +0100

    remove script.js

--- src/static/diary/add/script.js
+++
@@ -1,7 +0,0 @@
-const collapseButton = document.querySelector('.collapse-button')
-const memoryContainer = document.querySelector('.memory-container')
-
-collapseButton.addEventListener('click', () => {
-    memoryContainer.classList.toggle('hidden')
-    collapseButton.classList.toggle('switched')
-})
