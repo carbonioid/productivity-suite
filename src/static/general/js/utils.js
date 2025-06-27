d5c55b80e7f1c8f4559dbd66f9e8cf46a9bf19ad
BagelDogM <bageldogm@gmail.com>
2025-06-27 19:03:05 +0100

    refactor(general): refactor format_yyyy_mm_dd

--- src/static/general/js/utils.js
+++ src/static/general/js/utils.js
@@ -1,13 +1,10 @@
 export { format_yyyymmdd }
 
 function format_yyyymmdd(string, includeYear) {
-    let [year, month, day] = string.split('-');
-    // We ignore the year for now
-    const months = [
-      "Jan", "Feb", "Mar", "Apr", "May", "June",
-      "July", "Aug", "Sept", "Oct", "Nov", "Dec"
-    ];
-    month = months[Number(month)-1]
-    return includeYear ? `${Number(day)} ${month} ${year}` : `${Number(day)} ${month}`
+  let [year, month, day] = string.split('-');
+  const date = new Date(year, month - 1, day)
+  return date.toLocaleDateString(undefined, {
+    'month': 'short',
+    'day': '2-digit'
+  })
 }
-  
