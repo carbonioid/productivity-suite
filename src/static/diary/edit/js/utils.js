372e19f835ae9f0622a534707e05eaa79e24ebe5
BagelDogM <bageldogm@gmail.com>
2025-06-28 17:34:13 +0100

    refactor(diary): move dashboard redirect to util function

--- src/static/diary/edit/js/utils.js
+++ src/static/diary/edit/js/utils.js
@@ -1,4 +1,4 @@
-export {getDateMinusDays, getPageDate}
+export {getDateMinusDays, getPageDate, dashboardRedirect }
 
 function getDateMinusDays(date, days) {
     const newDate = new Date();
@@ -10,4 +10,12 @@ function getPageDate() {
     const urlParams = new URLSearchParams(window.location.search);
     const date = urlParams.get('date');
     return date || new Date().toISOString().split('T')[0]; // Default to today if no date is provided, in YYYY-MM-DD format
+}
+
+function dashboardRedirect() {
+    // clear url params and redirect to dashboard
+    window.history.replaceState(null, '', '/diary');
+
+    // Reload the window (otherwise the dashboard will not actually be redirected to)
+    window.location.reload();
 }
