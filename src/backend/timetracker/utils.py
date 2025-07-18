cd711010630abd55575d234a54b6dedc725dc6e2
BagelDogM <bageldogm@gmail.com>
2025-07-18 14:35:19 +0100

    refactor(timetracker): refactor file structure

--- src/backend/timetracker/utils.py
+++ src/backend/timetracker/utils.py
@@ -1,6 +1,10 @@
 import os
+from pathlib import Path
 from datetime import datetime
 from backend.common.utils import missing_dates
+
+DIR_PATH = 'src/backend/timetracker/data/'
+pathto = lambda name: Path(DIR_PATH, f'{name}.csv')
 
 def hhmm_to_minutes(string):
     hours, minutes = map(int, string.split(':'))
@@ -12,10 +16,10 @@ def add_new_files():
     Format: YYYY-MM-DD.csv
     """
     # List comprehension that gets all .csv files and then gets the most recent one with [-1]
-    dates = [os.path.splitext(file)[0] for file in os.listdir('src/backend/timetracker/res') if os.path.splitext(file)[1] == '.csv']
+    dates = [os.path.splitext(file)[0] for file in os.listdir(DIR_PATH) if os.path.splitext(file)[1] == '.csv']
     current_date = str(datetime.today().date())
 
     missing = missing_dates(dates, end=current_date)
 
     for date in missing:
-        open(f'src/backend/timetracker/res/{date}.csv', 'x') # Create file; it doesn't exist
+        open(pathto(date), 'x') # Create file; it doesn't exist
