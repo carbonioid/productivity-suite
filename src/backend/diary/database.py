0d46c0e88c2b385e5d9206ccc82da6124654e2d1
BagelDogM <bageldogm@gmail.com>
2025-07-19 10:32:26 +0100

    fix(diary): remove unnecessary logging

--- src/backend/diary/database.py
+++ src/backend/diary/database.py
@@ -69,7 +69,6 @@ def update_tag_index(tags, remove=False):
             change = -1 if remove else 1
             new_value = file_content.get(tag, 0)+change # account for the tag not yet tracked using get()
             
-            print(new_value)
             if new_value == 0: del file_content[tag]
             elif new_value > 0: file_content[tag] = new_value
             else: print("WARNING: tag index resolved to negative number. Tag index is likely not up to date.")
