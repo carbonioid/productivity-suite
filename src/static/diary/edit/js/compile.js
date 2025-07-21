01836beafb5e5951374e9081311a0196a322c24f
BagelDogM <bageldogm@gmail.com>
2025-07-21 15:18:46 +0100

    feat(diary): move edit actions to sticky navbar

--- src/static/diary/edit/js/compile.js
+++ src/static/diary/edit/js/compile.js
@@ -10,7 +10,7 @@ import { format_date } from "../../../general/js/utils.js";
 import { initDeleteButtonListeners } from "./listeners.js";
 import { loadTagInput } from "../../js/tag-select.js";
 import { setSelectedTags } from "../../js/tag-select.js";
-export { loadStatsButton, loadPageContent }
+export { loadPageContent }
 
 async function loadSlider(container, data) {
     /*
@@ -33,68 +33,57 @@ async function loadSlider(container, data) {
     container.appendChild(sliderTemplate);
 }
 
-function loadStatsButton(date) {
-    // Set date on "see stats" button
-    const statsDate = document.querySelector(".stats-button").querySelector(".stats-date");
-    statsDate.outerHTML = format_date(date, "medium");
-}
-
 async function loadPageContent(date) {
     /*
     Loads the page content for the given date.
     This includes loading the entry, sliders, tags, and stats button.
     Takes `date` in YYYY-MM-DD format.
-
-    It follows these steps:
-    (0) Populate title span with formatted date
-    (1) Get entry data
-    (2) Load entry & title & page title
-    (3) Load tags
-    (4) Load sliders
-    (5) Load stats button
-    (6) Conditionally load delete button if entry exists
     */
 
-    // Page title - populate span
+    // Populate date in page title
     const valueSpan = document.querySelector('.title-date-value');
     valueSpan.textContent = format_date(date, "medium");
 
-    // (0) Get entry data
+    // Load tags
+    await loadTagInput(document.querySelector(".tag-select"), true)
+
+    // Get entry data
     const entry = await getEntry(date, false);
+
+    // If entry is new, then run a smaller subset of commands
     if (entry == undefined || entry.empty) {
         console.info(`No entry found for date: ${date}. Loading empty page.`);
+        
         // Load sliders anyway, so that the user can add a new entry
+        // but use settings defaults because there are no values yet
         const settings = await getSettings();
         const container = document.querySelector('.rating-container');
         settings.ratings.forEach(rating => {
             loadSlider(container, rating)
         })
-
-        loadStatsButton(date)
-        return;
     }
+    // Otherwise, run behaviour for editing
+    else {
 
-    // (1) Load entry & title & page title
-    const entryInput = document.querySelector('.entry-input');
-    const titleInput = document.querySelector('.entry-title');
-    titleInput.value = entry.title || "";
-    entryInput.value = entry.entry || "";
+        // Load entry & title & page title
+        const entryInput = document.querySelector('.entry-input');
+        const titleInput = document.querySelector('.entry-title');
+        titleInput.value = entry.title || "";
+        entryInput.value = entry.entry || "";
 
-    // (2) Load tags
-    await loadTagInput(document.querySelector(".tag-select"), true)
-    setSelectedTags(entry.tags, document.querySelector(".tag-select"))
+        // Select tags
+        setSelectedTags(entry.tags, document.querySelector(".tag-select"))
 
-    // (3) Load sliders
-    const container = document.querySelector('.rating-container');
-    entry.ratings.forEach(rating => {
-        loadSlider(container, rating)
-    })
+        // Load sliders
+        const container = document.querySelector('.rating-container');
+        entry.ratings.forEach(rating => {
+            loadSlider(container, rating)
+        })
 
-    // (4) Load stats button
-    loadStatsButton(date);
-
-    // (5) Conditionally load delete button if entry exists (if we got to this point, we know we are editing so the button should appear)
-    const deleteButton = document.querySelector('.delete-button');
-    deleteButton.classList.remove('hidden');
-    initDeleteButtonListeners();
+        // Conditionally load delete button if entry exists
+        // (if we got to this point, we know we are editing so the button should appear, becuase otherwise the "new entry" thing would've been tripped and the function would've returned)
+        const deleteButton = document.querySelector('.delete-button');
+        deleteButton.classList.remove('hidden');
+        initDeleteButtonListeners();
+    }   
 }
