9a788aed754054a187d60e60d05b2f48b00f8fd5
BagelDogM <bageldogm@gmail.com>
2025-07-22 13:17:52 +0100

    feat: improve display options api

--- src/static/general/js/display.js
+++ src/static/general/js/display.js
@@ -43,19 +43,15 @@ class ClassWatcher {
 
 function initDisplayOption(targetNode, targetClass, targetFunction, triggerNode, triggerClass) {
     // Create new watcher for mutation events
-    let _  = new ClassWatcher(targetNode, targetClass, (hasClass) => {
-        if (hasClass) triggerNode.classList.add(triggerClass)
-        else          triggerNode.classList.remove(triggerClass)
-    })
+    if (targetClass) {
+        let _  = new ClassWatcher(targetNode, targetClass, (hasClass) => {
+            if (hasClass) triggerNode.classList.add(triggerClass)
+            else          triggerNode.classList.remove(triggerClass)
+        })
+    }
 
     triggerNode.addEventListener("click", () => {
-        // Run triger function if exists.
-        // Invert the value because we want to give the value for what
-        // the state will be after this click, not before.
-        if (targetFunction) {
-            targetFunction(!Boolean(triggerNode.classList.contains(triggerClass)))
-        }
-
+        const state = triggerNode.classList.contains(triggerClass)
         // Add and remove class from targetNode - this will trigger the dom watcher, 
         // which will in turn modify the classes for this button.
         // This is so that any mutations on these classes are automatically 
@@ -67,6 +63,19 @@ function initDisplayOption(targetNode, targetClass, targetFunction, triggerNode,
             } else {
                 targetNode.classList.add(targetClass)
             }
+        } else {
+            // If we aren't editing a class, then the mutation observer will not fire
+            // which means that the trigerNode's class will not be changed automaticaly. Therefore,
+            // we have to do this manually.
+            triggerNode.classList.toggle(triggerClass)
+        }
+
+        // Run triger function if exists.
+        // Invert the value because we want to give the value for what
+        // the state will be after this click, not before.
+        // (and the state doesn't reliably update, so we use the one we saved earlier.)
+        if (targetFunction) {
+            targetFunction(!state)
         }
     })
 
@@ -96,7 +105,7 @@ function init(config) {
     for (const row of config) {
         // First, validate that all needed config is present.
         if (!row.targetNode || !row.triggerNode || !(row.targetClass || row.targetFunction)) {
-            console.error(`Please ensure all options are passed correctly to display options config (offending row: ${row}`)
+            console.error(`Please ensure all options are passed correctly to display options config (offending row: ${JSON.stringify(row)})`)
             return;
         }
 
